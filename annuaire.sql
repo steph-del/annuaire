96eb6cb90e9f19dfe25bba9a47e73a94fc23d075
Stephane <dev@phytoscopa.fr>
2020-05-13 13:45:24 +0200

    update annuaire.sql

--- annuaire.sql
+++ annuaire.sql
@@ -26,10 +26,17 @@ CREATE TABLE `usermeta` (
 ) ENGINE=InnoDB AUTO_INCREMENT=891313 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_pseudo`, `user_nom`, `user_prenom`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`, `avatar_url`) VALUES
-(1, 'demo-user@example.org', '78838781849b8b78bc97403dfa2a327a', 'demo-user', 'Demo-User', 'Demo', 'User', 'demo-user@example.org', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'demo-user', 'avatar_url_value'),
-(2, 'stephane@phytoscopa.fr', '78838781849b8b78bc97403dfa2a327a', 'Stephane', 'Stéphane Delplanque', 'Delplanque', 'Stéphane', 'stephane@phytoscopa.fr', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'Stéphane', 'avatar_url_value'),
-(3, 'user@mail.com', '78838781849b8b78bc97403dfa2a327a', 'User 3', 'User 3', 'User', '3', 'user@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 3', 'avatar_url_value'),
-(4, 'admin@veglab.net', '78838781849b8b78bc97403dfa2a327a', 'Admin', 'Admin', 'Admin', '', 'admin@veglab.net', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'admin', 'avatar_url_value');
+(1, 'demo-user@example.org', '5b7998ce32d84abe6dac087e33cd07a9', 'demo-user', 'Demo-User', 'Demo', 'User', 'demo-user@example.org', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'demo-user', 'avatar_url_value'),
+(2, 'stephane@phytoscopa.fr', '084de89f5548dc286881da64f89e3d80', 'Stephane', 'Stéphane Delplanque', 'Delplanque', 'Stéphane', 'stephane@phytoscopa.fr', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'Stéphane', 'avatar_url_value'),
+(3, 'user1@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 1', 'User 1', 'User', '1', 'user1@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 1', 'avatar_url_value'),
+(4, 'user2@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 2', 'User 2', 'User', '2', 'user2@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 2', 'avatar_url_value'),
+(5, 'user3@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 3', 'User 3', 'User', '3', 'user3@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 3', 'avatar_url_value'),
+(6, 'user4@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 4', 'User 4', 'User', '4', 'user4@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 4', 'avatar_url_value'),
+(7, 'user5@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 5', 'User 5', 'User', '5', 'user5@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 5', 'avatar_url_value'),
+(8, 'user6@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 6', 'User 6', 'User', '6', 'user6@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 6', 'avatar_url_value'),
+(9, 'user7@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 7', 'User 7', 'User', '7', 'user7@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 7', 'avatar_url_value'),
+(10, 'user8@mail.com', '5b7998ce32d84abe6dac087e33cd07a9', 'User 8', 'User 8', 'User', '8', 'user8@mail.com', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'user 8', 'avatar_url_value'),
+(11, 'admin@veglab.net', '5b7998ce32d84abe6dac087e33cd07a9', 'Admin', 'Admin', 'Admin', '', 'admin@veglab.net', '', '2016-10-17 12:30:45', '1507884852:$P$B8nQgST869NUKyTjxiC9P1sokhi3MY0', 0, 'admin', 'avatar_url_value');
 
 INSERT INTO `usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) VALUES
 (1, 1, 'avatar_url', 'url_avatar_value'),
